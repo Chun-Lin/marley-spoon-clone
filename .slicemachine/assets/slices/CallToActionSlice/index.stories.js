ef5a2368d915cb7386b409a4c586bce78f1da315
Chun-Lin <wulin40063@gmail.com>
2022-03-14 19:57:39 +0800

    feat: 🎸 CallToActionSlice Layout

--- .slicemachine/assets/slices/CallToActionSlice/index.stories.js
+++ .slicemachine/assets/slices/CallToActionSlice/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"call_to_action_slice","items":[],"primary":{"title":[{"type":"heading2","text":"Visualize user-centric convergence","spans":[]}],"signupButton":{"link_type":"Web","url":"http://google.com"},"bookDemoButton":{"link_type":"Web","url":"https://slicemachine.dev"}},"id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"call_to_action_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Drive proactive mindshare","spans":[]}],"ctaBtnName":[{"type":"paragraph","text":"Ad occaecat non incididunt labore ad enim duis aute incididunt cillum exercitation officia aliqua. Nostrud irure in eiusmod occaecat adipisicing laboris voluptate labore nisi aliquip minim consequat pariatur.","spans":[]}],"signupButtonLink":{"link_type":"Web","url":"http://twitter.com"},"bookDemoDesc":[{"type":"paragraph","text":"Fugiat nulla velit excepteur nisi ea ad laboris id.","spans":[]}]},"id":"_Default"}} />
 _Default.storyName = 'Default'
--- slices/CallToActionSlice/index.js
+++ slices/CallToActionSlice/index.js
@@ -1,31 +1,88 @@
-import React from 'react'
-import { PrismicRichText } from '@prismicio/react'
+import React from 'react';
+import { PrismicRichText, PrismicText } from '@prismicio/react';
+import styled from 'styled-components';
 
-const CallToActionSlice = ({ slice }) => (
-  <section>
-    <span className="title">
-      {
-        slice.primary.title ?
-        <PrismicRichText field={slice.primary.title}/>
-        : <h2>Template slice, update me!</h2>
-      }
-    </span>
-    {
-      slice.primary.description ?
-      <PrismicRichText field={slice.primary.description}/>
-      : <p>start by editing this slice from inside Slice Machine!</p>
-    }
-    <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
-    `}</style>
-  </section>
-)
+const CallToActionSection = styled.section`
+  background-color: #152231;
+`;
 
-export default CallToActionSlice
+const CtaWrapper = styled.div`
+  max-width: 1080px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 80px;
+  padding-bottom: 80px;
+`;
+
+const CtaTitle = styled.h2`
+  max-width: 440px;
+  margin-bottom: 40px;
+  font-size: 2rem;
+  line-height: 1.2em;
+  text-align: center;
+  color: #fff;
+  font-weight: 300;
+
+  strong {
+    font-weight: 600;
+  }
+`;
+
+const CtaButton = styled.button`
+  max-width: 90%;
+  width: 335px;
+  padding: 14px 36px;
+  border-radius: 5px;
+  background-color: #46b4e9;
+  background-image: linear-gradient(#46b4e9, #0070af);
+  color: #fff;
+  font-size: 1.16rem;
+  font-weight: 600;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #46b4e9;
+    background-image: linear-gradient(#46b4e9, #005788);
+  }
+`;
+
+const BookDemoTextWrapper = styled.p`
+  margin-top: 10px;
+  font-size: 18px;
+  color: #fff;
+
+  a {
+    text-decoration: none;
+    color: #46b4e9;
+  }
+`;
+
+const CallToActionSlice = ({ slice }) => {
+  const { primary } = slice;
+  const { title, ctaBtnName, signupButtonLink, bookDemoDesc } = primary;
+
+  const ctaBtnClickHandler = (event) => {
+    window.location.href = signupButtonLink.url;
+  };
+
+  return (
+    <CallToActionSection>
+      <CtaWrapper>
+        <CtaTitle>
+          <PrismicRichText field={title} />
+        </CtaTitle>
+        <CtaButton onClick={ctaBtnClickHandler}>
+          <PrismicRichText field={ctaBtnName} />
+        </CtaButton>
+        <BookDemoTextWrapper>
+          <PrismicRichText field={bookDemoDesc} />
+        </BookDemoTextWrapper>
+      </CtaWrapper>
+    </CallToActionSection>
+  );
+};
+
+export default CallToActionSlice;
