d012f39e9186477ca6678e8f70371f2fb1b7f52a
Chun-Lin <wulin40063@gmail.com>
2022-03-20 20:33:57 +0800

    style: 💄 adjust the responsiveness of banner iamge

--- slices/BannerSlice/index.js
+++ slices/BannerSlice/index.js
@@ -3,23 +3,22 @@ import { PrismicRichText } from '@prismicio/react';
 import styled from 'styled-components';
 import Image from 'next/image';
 
-const BannerImage = styled(Image)`
-  width: 100vw;
-  object-fit: cover;
+const BannerSection = styled.section`
+  position: relative;
+  height: 450px;
 `;
 
 const BannerSlice = ({ slice }) => {
   const { url, alt, dimensions } = slice.primary.bannerImage;
   return (
-    <section>
-      <BannerImage
+    <BannerSection>
+      <Image
         src={slice.primary.bannerImage.url}
         alt={slice.primary.bannerImage.alt}
         objectFit="cover"
-        width={dimensions.width}
-        height={dimensions.height}
+        layout="fill"
       />
-    </section>
+    </BannerSection>
   );
 };
 
