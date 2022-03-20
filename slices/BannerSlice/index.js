6524a35bbeab0b924bbc6acf42c757944d6fa8fb
Chun-Lin <wulin40063@gmail.com>
2022-03-20 20:24:37 +0800

    perf: ⚡️ replace banner img html with next/Image

--- slices/BannerSlice/index.js
+++ slices/BannerSlice/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { PrismicRichText } from '@prismicio/react';
 import styled from 'styled-components';
+import Image from 'next/image';
 
-const BannerImage = styled.img`
+const BannerImage = styled(Image)`
   width: 100vw;
   object-fit: cover;
 `;
 
-const BannerSlice = ({ slice }) => (
-  <section>
-    <BannerImage
-      src={slice.primary.bannerImage.url}
-      alt={slice.primary.bannerImage.alt}
-    />
-  </section>
-);
+const BannerSlice = ({ slice }) => {
+  const { url, alt, dimensions } = slice.primary.bannerImage;
+  return (
+    <section>
+      <BannerImage
+        src={slice.primary.bannerImage.url}
+        alt={slice.primary.bannerImage.alt}
+        objectFit="cover"
+        width={dimensions.width}
+        height={dimensions.height}
+      />
+    </section>
+  );
+};
 
 export default BannerSlice;
